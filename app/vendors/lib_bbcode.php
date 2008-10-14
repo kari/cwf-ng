2786bde0cb3c52756b033ef6800af16416846d9c
Kari Silvennoinen <kari@users.noreply.github.com>
2008-10-14 13:21:53 +0300

    BBCode decoding support, random fixes
    committer: Kari Silvennoinen <kari@kalifi.org>

---
+++ app/vendors/lib_bbcode.php
@@ -0,0 +1,82 @@
+<?php 
+/* 
+Originally from http://www.phpit.net/code/bbcode-format-function/
+
+*/ 
+
+function bbcode_format ($str) { 
+    $str = htmlentities($str,ENT_QUOTES); // ENT_QUOTES added by zyx
+
+    $simple_search = array( 
+                '/\[b\](.*?)\[\/b\]/is',                                 
+                '/\[i\](.*?)\[\/i\]/is',                                 
+                '/\[u\](.*?)\[\/u\]/is',                                 
+                '/\[url\=(.*?)\](.*?)\[\/url\]/is',                          
+                '/\[url\](.*?)\[\/url\]/is',                              
+                '/\[align\=(left|center|right)\](.*?)\[\/align\]/is',     
+                '/\[img\](.*?)\[\/img\]/is',
+                '/\[img\=(.*?)\](.*?)\[\/img\]/is', // added by zyx  
+                '/\[mail\=(.*?)\](.*?)\[\/mail\]/is',                     
+                '/\[mail\](.*?)\[\/mail\]/is',                             
+                '/\[font\=(.*?)\](.*?)\[\/font\]/is',                     
+                '/\[size\=(.*?)\](.*?)\[\/size\]/is',                     
+                '/\[color\=(.*?)\](.*?)\[\/color\]/is',         
+                ); 
+
+    $simple_replace = array( 
+                '<strong>$1</strong>', 
+                '<em>$1</em>', 
+                '<u>$1</u>', 
+                '<a href="$1">$2</a>', 
+                '<a href="$1">$1</a>', 
+                '<div style="text-align: $1;">$2</div>', 
+                '<img src="$1" />',
+								'<img src="$1" title="$2" />', // added by zyx
+                '<a href="mailto:$1">$2</a>', 
+                '<a href="mailto:$1">$1</a>', 
+                '<span style="font-family: $1;">$2</span>', 
+                '<span style="font-size: $1;">$2</span>', 
+                '<span style="color: $1;">$2</span>', 
+                ); 
+
+    // Do simple BBCode's 
+    $str = preg_replace ($simple_search, $simple_replace, $str); 
+
+    // Do <blockquote> BBCode 
+    $str = bbcode_quote ($str); 
+    
+    // Do nl2br
+    
+    $str = nl2br($str); // added by zyx
+
+    return $str; 
+} 
+
+
+
+function bbcode_quote ($str) { 
+    $open = '<blockquote>'; 
+    $close = '</blockquote>'; 
+
+    // How often is the open tag? 
+    preg_match_all ('/\[quote\]/i', $str, $matches); 
+    $opentags = count($matches['0']); 
+
+    // How often is the close tag? 
+    preg_match_all ('/\[\/quote\]/i', $str, $matches); 
+    $closetags = count($matches['0']); 
+
+    // Check how many tags have been unclosed 
+    // And add the unclosing tag at the end of the message 
+    $unclosed = $opentags - $closetags; 
+    for ($i = 0; $i < $unclosed; $i++) { 
+        $str .= '</blockquote>'; 
+    } 
+
+    // Do replacement 
+    $str = str_replace ('[' . 'quote]', $open, $str); 
+    $str = str_replace ('[/' . 'quote]', $close, $str); 
+
+    return $str; 
+} 
+?>
