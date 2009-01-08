b23fb85728cd6fdac27233a7e8a92993a4353578
Kari Silvennoinen <kari@kalifi.org>
2009-01-08 10:55:19 +0200

    Added reCAPTCHA to unregistered comments. games/view goes over 8M memory limit.
    committer: Kari Silvennoinen <kari@kalifi.org>

--- app/controllers/comments_controller.php
+++ app/controllers/comments_controller.php
@@ -9,11 +9,16 @@ class CommentsController extends AppController {
 			# "fields" => array("game_name","game_id"),
 			'recursive' => 1
 	    );
+	
+	var $components = array('Recaptcha');
+	
 
 	function beforeFilter() {
 		parent::beforeFilter();
 		$this->Auth->allow(array("add")); # FIXME: No comments/index in production.
 		$this->Auth->mapActions(array("queue"=>"admin"));
+		$this->Recaptcha->publickey = Configure::read("Site.captcha_public_key"); 
+		$this->Recaptcha->privatekey = Configure::read("Site.captcha_private_key");
 	}
 	
 	function queue() {
@@ -29,7 +34,11 @@ class CommentsController extends AppController {
 			if ($this->Auth->user()) { 
 				$this->data["Comment"]["validated"] = true; 
 				$this->data["Comment"]["user_id"] = $this->Auth->user("user_id");
-			} else {
+			} elseif(!$this->Recaptcha->valid($this->params['form'])) {
+					$this->Session->setFlash("Comment wasn't added, because reCAPTCHA was wrong. Please try again.");
+					# FIXME: Form contents are not saved, causing form to be empty!
+					$this->redirect($this->referer());
+			} elseif($this->Recaptcha->valid($this->params['form'])) {
 				$this->data["Comment"]["validated"] = false;
 				$this->data["Comment"]["user_id"] = -1; # FIXME: Magic number
 			}
