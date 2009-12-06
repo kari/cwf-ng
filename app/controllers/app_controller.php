36bc52fd703ff62ee5f7c8b5f5ae1d9946a93a83
Kari Silvennoinen <kari@users.noreply.github.com>
2009-12-06 20:10:05 +0200

    Now with phpBB2 cookie auto-login.

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -23,13 +23,45 @@ class AppController extends Controller {
 		
 		# $this->RememberMe->check(); # FIXME: Remove rememberme
 		
-		# If user is not logged in according to AuthComponent, check if phpBB cookie exists, fetch user's password from database and do a manual login.
-		
-/*		if (!$this->Auth->user()) {
-			$phbbp_cookie = unserialize($_COOKIE["CWmysql_data"]);
-			$authloginid = $phpbb_cookie["authloginid"];
-			$authloginkey = md5($authloginid);
-		} */
+		# If user is not logged in according to AuthComponent, check 
+		# if phpBB cookie exists, fetch user's password from database and 
+		# do a manual login.		
+		if (!$this->Auth->user()) {
+			if (!empty($_COOKIE["CWmysql_data"]) && !empty($_COOKIE["CWmysql_sid"])) {
+				/* Cookie exists, so let's get autologin info */
+				$phpbb_cookie = unserialize(stripslashes($_COOKIE["CWmysql_data"])); # NB: stripslashes require if some php-setting is on (as it is on surftown) that adds slashes to the data. 
+				$authloginid = $phpbb_cookie["autologinid"];
+				$authuserid = $phpbb_cookie["userid"];
+				$session = $_COOKIE["CWmysql_sid"]; # We could also check that a valid session exists, but do/should we care?
+				
+				/* Does the cookie actually have any info we can use? */
+				if (!empty($authuserid) && !empty($authloginid)) {
+					$authloginkey = md5($authloginid);
+					/* Can we find a session key for this user? */
+					$query = 'SELECT * FROM phpbb_users AS u, phpbb_sessions_keys AS k WHERE u.user_id = "' . $authuserid . '" AND u.user_active = 1 AND u.user_id = k.user_id AND k.key_id = "'.$authloginkey.'"';
+					$result = $this->User->query($query);
+				
+					if (!empty($result)) {
+						/* We did find a valid session key, let's try to login */
+						$user = $this->User->find("first",array("conditions"=>array("user_id"=>$authuserid,'User.user_active' => '1'),"fields" => array($this->Auth->fields["username"],$this->Auth->fields["password"])));
+						if (!$this->Auth->login($user)) {
+							# $this->Session->setFlash("We could not login you.");
+							/* We probably need to redirect to phpbb's login thing. */
+						} else {
+							# $this->Session->setFlash("We could login you in. Fun times.");
+							/* CakePHP AuthComponent should now have logged us in. We're god to go */
+						}
+					} else {
+						# $this->Session->setFlash("No valid session key found.");
+					}
+				} else {
+					# $this->Session->setFlash("Not logged in or auto-login disabled.");
+				}
+			} else {
+				# $this->Session->setFlash("No cookie. You must be new around here.");
+			}
+		} 
+
 		
 		$controller = str_replace("_","",$this->params["controller"]); # FIXME: There's probably a function for formatting the controller name more robustly.
 		if ($this->User->isAuthorized($this->Auth->user(),$controller,"admin")) { # FIXME: Controller name here is not set by the same function as with authorization. Works, though.
